91272f0eb685ffe22ebaab45b8b7c13c54eeaa68
Mohammed Samy <118897322+Eanzo22@users.noreply.github.com>
2023-10-05 18:33:02 +0300

    added body to the drugs , orders ,and companies with tables and forms

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -265,13 +265,13 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-12">
-                                @yield('create_drugs')
+                                @yield('content')
+                            
+
                             </div>
                         </div>
 
                         <div class="row">
-
-
                         </div>
                         <div class="row">
 
